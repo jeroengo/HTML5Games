58db8c9b53c46f26ed19141819512bf316216c88
Jeroen <mjeroengorissen@gmail.com>
2023-11-04 16:36:57 +0100

    Added normalized player speed

--- VampireSurvivorClone/player.js
+++ VampireSurvivorClone/player.js
@@ -16,21 +16,11 @@ class Player {
 
     Update() {
 
-        // Update player position based on user input or game logic
-        if (keys.ArrowUp) {
-            this.player.y -= this.player.speed;
-        }
-        if (keys.ArrowDown) {
-            this.player.y += this.player.speed;
-        }
-        if (keys.ArrowLeft) {
-            this.player.x -= this.player.speed;
-        }
-        if (keys.ArrowRight) {
-            this.player.x += this.player.speed;
-        }
+        
 
+        this.PlayerMovement();
         this.Draw();
+
     }
 
      
@@ -42,6 +32,46 @@ class Player {
 
     getPlayerPostition() {
         return { x: this.player.x, y: this.player.y };
+    }
+
+    PlayerMovement(){
+        // Update player position based on user input or game logic
+
+        let playerspeed = {x:0,y:0};        
+
+        if (keys.ArrowUp) {
+            playerspeed.y = -this.player.speed;
+        }
+        if (keys.ArrowDown) {
+            playerspeed.y = this.player.speed;
+        }
+        if (keys.ArrowLeft) {
+            playerspeed.x = -this.player.speed;
+        }
+        if (keys.ArrowRight) {
+            playerspeed.x = this.player.speed;
+        }
+
+        if(playerspeed.x + playerspeed.y == 2){
+            playerspeed.x = playerspeed.x * 0.1;
+            playerspeed.y = playerspeed.y * 0.1; 
+        }
+        //als movement speed groter is dan 2
+
+        
+
+        //let distance = Math.sqrt(directionX * directionX + directionY * directionY);
+        let inputvector = Math.sqrt(playerspeed.x*playerspeed.x + playerspeed.y * playerspeed.y);
+
+        if(inputvector !== 0){
+           playerspeed.x /= inputvector;
+           playerspeed.y /= inputvector;
+        }
+
+
+
+        this.player.x += playerspeed.x * this.player.speed;
+        this.player.y += playerspeed.y * this.player.speed;
     }
 
 }
