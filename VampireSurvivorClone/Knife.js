babe1ac0fb636d9244fe88a01bf72d0550238663
Jeroen <mjeroengorissen@gmail.com>
2023-11-09 08:24:03 +0100

    Added self destruction logic with timer to knife

--- VampireSurvivorClone/Knife.js
+++ VampireSurvivorClone/Knife.js
@@ -2,37 +2,52 @@ class Knife {
     constructor(direction, startx, starty) {
         this.object = {
             width: 10,
-            height: 10,            
+            height: 10,
             x: startx,
-            y: starty,            
+            y: starty,
             speed: 15,
             damage: 1,
-            isAlive: true
+            isAlive: true,
+            lifetime: 50
         };
 
-        this.object.x -= this.object.width/2;
-        this.object.y -= this.object.height/2;
+        this.object.x -= this.object.width / 2;
+        this.object.y -= this.object.height / 2;
 
         this.dir = {
             x: direction.x,
             y: direction.y
         };
+
+        this.timer = 0;
     }
+
 
     Update() {
 
+        this.SelfDestruction();
         this.CheckCollision();
         this.Move();
         this.Draw();
-    }
-    
-    CheckCollision(){
-       MultiCollision(this.object, objManager.gameObjects);
+
     }
 
-    getAliveStatus(){
+    CheckCollision() {
+        MultiCollision(this.object, objManager.gameObjects);
+    }
+
+    SelfDestruction() {
+        this.timer++;
+
+        if (this.timer > this.object.lifetime) {
+            this.object.isAlive = false;
+            //console.log("Im going down!");
+        }
+    }
+
+    getAliveStatus() {
         return this.object.isAlive;
-       }
+    }
 
     Move() {
         this.object.x += this.dir.x * this.object.speed;
