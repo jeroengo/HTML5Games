5e000f76c794cfdab86cf5b9b9410900010a63aa
Jeroen <mjeroengorissen@gmail.com>
2023-11-27 17:49:30 +0100

    Fixed error in calculate distance check for the coin

--- VampireSurvivorClone/Weapons/Coin.js
+++ VampireSurvivorClone/Weapons/Coin.js
@@ -5,8 +5,13 @@ class Coin {
             height: 10,
             x: startx,
             y: starty,
-            isAlive: true
+            isAlive: true,
+            inRange: false,
+            speed: 5,
+            type: 'coin'
         };
+
+        let distance = 0;
 
         this.object.x -= this.object.width / 2;
         this.object.y -= this.object.height / 2;
@@ -15,15 +20,40 @@ class Coin {
 
     Update() {
 
-        this.CheckCollision();
-        this.Move();
+            // console.log("D" + this.CalculateDistance(this.object.x, this.object.y,
+            //     newPlayer.getPlayer().x + newPlayer.getPlayer().width / 2,
+            //     newPlayer.getPlayer().y + newPlayer.getPlayer().height / 2));
+
+            if (this.CalculateDistance(this.object.x, this.object.y,
+                newPlayer.getPlayer().x + newPlayer.getPlayer().width / 2,
+                newPlayer.getPlayer().y + newPlayer.getPlayer().height / 2) <= 150)
+                {
+                this.object.inRange = true;
+            }
+        
+        if (this.object.inRange == true) {
+
+            this.moveTowardsPlayer();
+        }
+
         this.Draw();
+        this.CheckCollision();
     }
 
-    CheckCollision() {
-        
 
-        if(Collision(this.object, newPlayer.player)){
+
+    CalculateDistance(x1, y1, x2, y2) {
+        const deltaX = x2 - x1;
+        const deltaY = y2 - y1;
+        const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+        return distance;
+    }
+
+
+    CheckCollision() {
+
+        if (Collision(this.object, newPlayer.player)) {
             scoreManager.adjustScore(1);
             this.SelfDestruction();
         }
@@ -40,9 +70,35 @@ class Coin {
     Move() {
     }
 
+    moveTowardsPlayer() {
+
+        var playerpos = {
+            x: newPlayer.getPlayer().x + newPlayer.player.width / 2,
+            y: newPlayer.getPlayer().y + newPlayer.player.height / 2
+        };
+
+        let directionX = playerpos.x - this.object.x;
+        let directionY = playerpos.y - this.object.y;
+
+        let distance = Math.sqrt(directionX * directionX + directionY * directionY);
+
+        if (distance <= this.object.speed) {
+            this.object.x = playerpos.x;
+            this.object.y = playerpos.y;
+            return; // Character has reached the target
+        }
+
+        // Normalize the direction vector
+        let normalizedDirectionX1 = directionX / distance;
+        let normalizedDirectionY1 = directionY / distance;
+
+        // Update the character's position in the direction of the target
+        this.object.x += normalizedDirectionX1 * this.object.speed;
+        this.object.y += normalizedDirectionY1 * this.object.speed;
+    }
 
     Draw() {
-        ctx.fillStyle = "black";
+        ctx.fillStyle = "green";
         ctx.fillRect(this.object.x, this.object.y, this.object.width, this.object.height);
     }
 }
